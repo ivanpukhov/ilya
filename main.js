7e993ac8d115ca60a0547a44031b85e7e20e8d4b
ivan <ix.pukhov@gmail.com>
2023-01-27 23:06:59 +0600

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const login = (age) => {
+    if (age < 18){
+        return('Совершенолетний')
+    } else{
+        return('Несовершеннолетний')
+    }
+}
+
+const gap = (number, after, end) => {
+  if (number >= after && number <= end){
+      return 'да'
+    } else{
+      return 'нет'
+    }
+}
+
+
+
+const maxOfArray = (arr) => {
+    return Math.max.apply(null, arr)
+}
+
+const maxIndexOfArray = (arr) => {
+    return(arr.indexOf( Math.max.apply(null, arr)))
+}
+
